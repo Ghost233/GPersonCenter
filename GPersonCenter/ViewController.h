48aaae85b7427410611fce647af900813584c1ce
Ghost233 <only.yesc@gmail.com>
2016-03-23 14:52:46 +0800

    init

---
+++ GPersonCenter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GPersonCenter
+//
+//  Created by Ghost on 16/3/23.
+//  Copyright © 2016年 Ghost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
